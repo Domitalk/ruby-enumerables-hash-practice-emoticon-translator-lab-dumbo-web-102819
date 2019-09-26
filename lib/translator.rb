4b41271044ac691d98cc6cf4d9eeae96162fe657
Dominic Chu <dominic.m.chu@gmail.com>
2019-09-26 00:45:28 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,29 @@
 # require modules here
+require "yaml"
 
-def load_library
-  # code goes here
+def load_library(file)
+  simple_view = YAML.load_file(file)
+  new_hash = Hash.new
+  new_hash = {
+    "get_meaning" => {
+      #Japanese emoticons => "meaning",
+      #Japanese emotoicons => "meaning"
+    },
+    "get_emoticon" => {
+      #English emoticons => "meaning",
+      #English emoticons => "meaning"
+    }
+  }
+  simple_view.each do |key, value|
+    value.each_with_index do |emo, index|
+      if index == 0
+        new_hash["get_emoticon"][emo] = value[1]
+      elsif index == 1
+        new_hash["get_meaning"][emo] = key
+      end
+    end
+  end
+  new_hash
 end
 
 def get_japanese_emoticon
